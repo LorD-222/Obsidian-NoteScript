dc5094e11e3e9c45bab815ac1c024fc209f927e9
Rybakov <rybakov@gnivc.ru>
2023-06-17 18:32:02 +0300

    update RSS_to_markdown_cleanup

--- Scripts/automation/RSS_to_markdown_cleanup.py
+++ Scripts/automation/RSS_to_markdown_cleanup.py
@@ -5,25 +5,42 @@ from bs4 import BeautifulSoup
 import re
 
 
+# Функция для обработки текста статьи и извлечения ссылок
+def process_article_content(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    text_with_links = ''
+    for tag in soup.find_all(True):
+        if tag.name == 'p':
+            if tag.find('a'):
+                link = tag.a
+                text = tag.get_text()
+                text = text.replace(link.text, f"[{link.text}]({link.get('href')})")
+                text_with_links += text + '\n\n'
+            else:
+                text_with_links += tag.text + '\n\n'
+    return text_with_links
+
+
 # Функция для сохранения статьи в формате Markdown
 def save_article_as_md(entry, save_path):
-    # Получение заголовка статьи, удаление всех небуквенно-цифровых символов
     title = re.sub(r'[^\w\s]', '', entry.title)[:200]
     date = entry.published
-    content = entry.summary
-    soup = BeautifulSoup(content, 'html.parser')
-    text = soup.get_text()
-    filename = f"{title}.md"
+    content = process_article_content(entry.summary)
 
-    # Проверка категории в записи и создание директории с именем категории
-    if 'tags' in entry.keys():
-        if len(entry.tags) > 1:
-            category = entry.tags[1].term
-        else:
+    filename = f"{title}.md"
+    category = None
+    if 'tags' in entry.keys() and len(entry.tags) >= 1:
+        for tag in entry.tags[1:]:
+            if not tag.term.startswith('.'):
+                category = tag.term
+                break
+        if not category and not entry.tags[0].term.startswith('.'):
             category = entry.tags[0].term
-        category_path = os.path.join(save_path, category)
-        if not os.path.exists(category_path):
-            os.makedirs(category_path)
+        if category:
+            category_path = os.path.join(save_path, category)
+            os.makedirs(category_path, exist_ok=True)
+        else:
+            category_path = save_path
     else:
         category_path = save_path
 
@@ -32,7 +49,7 @@ def save_article_as_md(entry, save_path):
         f.write(f"# {title}\n")
         f.write(f"> Published on {date}\n")
         f.write("\n---\n")
-        f.write(f"{text}\n")
+        f.write(f"{content}\n")
 
 
 # Функция для получения RSS-ленты и вызова функции сохранения
@@ -42,7 +59,7 @@ def get_rss_feed(url, save_path):
         save_article_as_md(entry, save_path)
 
 
-# Функция дляудаления старых файлов и пустых папок старше недели
+# Функция для удаления старых файлов и пустых папок старше недели
 def delete_old_files_and_empty_folders(path, days_old=7):
     now = time.time()
     for root, dirs, files in os.walk(path, topdown=False):
@@ -58,7 +75,8 @@ def delete_old_files_and_empty_folders(path, days_old=7):
 
 if __name__ == "__main__":
     url = "https://tproger.ru/feed/"  # URL RSS-ленты
-    base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    file_path = os.path.realpath(__file__)
+    base_dir = os.path.dirname(os.path.dirname(os.path.dirname(file_path)))
     save_path = os.path.join(base_dir, 'RSS')
     delete_old_files_and_empty_folders(save_path)
     get_rss_feed(url, save_path)
